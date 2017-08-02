ffae05aaf1c747fab801c955d87ab0262b38fa21
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2017-08-02 18:24:24 +0200

    Fixing `_.max` conversion

--- check.py
+++ check.py
@@ -70,7 +70,7 @@ CONVERTED_FROM_UNDERSCORE = {
   '_.last': ['_.last', '_.takeRight'],
   '_.map': ['_.map', '_.map(_.bind)'],
   '_.mapObject': ['_.mapValues', '_.mapValues(_.bind)'],
-  '_.max': ['_.last', '_.last(_.bind)'],
+  '_.max': ['_.maxBy', '_.maxBy(_.bind)'],
   '_.min': ['_.minBy', '_.minBy(_.bind)'],
   '_.object': ['_.zipObject'],
   '_.omit': ['_.omit', '_.omitBy'],
